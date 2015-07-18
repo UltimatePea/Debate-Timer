1cc1e9aca3e1f6cb980971731ea4ac350e14503d
Zhibo Chen <a1s1d1f1j1k1l1@126.com>
2015-07-18 20:36:47 +0800

    Initial Commit

---
+++ Debate Timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Debate Timer
+//
+//  Created by Chen Zhibo on 7/18/15.
+//  Copyright © 2015 Chen Zhibo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Debate Timer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Debate Timer
+//
+//  Created by Chen Zhibo on 7/18/15.
+//  Copyright © 2015 Chen Zhibo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
